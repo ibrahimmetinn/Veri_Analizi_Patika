b841337ae24e6e5c3c7475a9bb9ac0ae6413b818
İbrahim Metin <150549575+ibrahimmetinn@users.noreply.github.com>
2024-01-22 12:59:04 +0300

    Update and Delete
    POSTGRESQL üzerinden veri güncelleme  ve veri silme ödevi

---
+++ PostgreSQL_Missions/ÖDEV 8 (UPDATE AND DELETE).sql
@@ -0,0 +1,133 @@
+--SELECT * FROM employes
+/*insert into employes (id, name, e_mail, birthday) values (1, 'Yevette', 'yperotti0@studiopress.com', '1908/07/31');
+insert into employes (id, name, e_mail, birthday) values (2, 'Dion', 'dkedie1@bloglovin.com', '1910/06/18');
+insert into employes (id, name, e_mail, birthday) values (3, 'Aila', 'adanielovitch2@newyorker.com', '1905/11/15');
+insert into employes (id, name, e_mail, birthday) values (4, 'Olvan', 'ostratford3@odnoklassniki.ru', '1933/11/19');
+insert into employes (id, name, e_mail, birthday) values (5, 'Debby', 'dschettini4@flickr.com', '1943/04/04');
+insert into employes (id, name, e_mail, birthday) values (6, 'Pennie', 'pcartmell5@paypal.com', '1999/10/28');
+insert into employes (id, name, e_mail, birthday) values (7, 'Reuven', 'rwhitwell6@weibo.com', '1982/05/04');
+insert into employes (id, name, e_mail, birthday) values (8, 'Alfy', 'adeleon7@fastcompany.com', '1931/12/13');
+insert into employes (id, name, e_mail, birthday) values (9, 'Kirstin', 'kblissitt8@ehow.com', '1959/11/17');
+insert into employes (id, name, e_mail, birthday) values (10, 'Shepard', 'scobb9@flavors.me', '1977/06/15');
+insert into employes (id, name, e_mail, birthday) values (11, 'Godart', 'gtregiddoa@senate.gov', '1999/02/15');
+insert into employes (id, name, e_mail, birthday) values (12, 'Sybyl', 'skolyaginb@trellian.com', '1951/01/12');
+insert into employes (id, name, e_mail, birthday) values (13, 'Leilah', 'lswanstonc@mlb.com', '1940/02/16');
+insert into employes (id, name, e_mail, birthday) values (14, 'Marie-ann', 'mchiened@cdbaby.com', '1916/08/08');
+insert into employes (id, name, e_mail, birthday) values (15, 'Dalenna', 'dmerrigane@discovery.com', '1906/06/25');
+insert into employes (id, name, e_mail, birthday) values (16, 'Virginia', 'vcoasterf@fotki.com', '1908/06/26');
+insert into employes (id, name, e_mail, birthday) values (17, 'Shelby', 'slangrickg@squidoo.com', '1946/11/18');
+insert into employes (id, name, e_mail, birthday) values (18, 'Thurstan', 'tbloggh@berkeley.edu', '1928/07/10');
+insert into employes (id, name, e_mail, birthday) values (19, 'Ronica', 'rcominsi@timesonline.co.uk', '1979/03/12');
+insert into employes (id, name, e_mail, birthday) values (20, 'Alida', 'atrowlej@g.co', '1955/08/07');
+insert into employes (id, name, e_mail, birthday) values (21, 'Nell', 'nmilesapk@theglobeandmail.com', '1977/06/01');
+insert into employes (id, name, e_mail, birthday) values (22, 'Kali', 'kbooml@tiny.cc', '1962/11/05');
+insert into employes (id, name, e_mail, birthday) values (23, 'Kym', 'kbordesm@vinaora.com', '1904/05/11');
+insert into employes (id, name, e_mail, birthday) values (24, 'Rebecca', 'rdecroixn@lycos.com', '1984/09/27');
+insert into employes (id, name, e_mail, birthday) values (25, 'Hermia', 'hkleemano@ustream.tv', '1944/01/16');
+insert into employes (id, name, e_mail, birthday) values (26, 'Etty', 'eheiferp@dot.gov', '1964/11/26');
+insert into employes (id, name, e_mail, birthday) values (27, 'Standford', 'sdecristofaloq@bigcartel.com', '1914/03/10');
+insert into employes (id, name, e_mail, birthday) values (28, 'Paloma', 'pwakemanr@stanford.edu', '1971/01/29');
+insert into employes (id, name, e_mail, birthday) values (29, 'Baillie', 'bkingsmills@wikimedia.org', '1923/09/21');
+insert into employes (id, name, e_mail, birthday) values (30, 'Nikolos', 'npasst@smh.com.au', '1927/09/27');
+insert into employes (id, name, e_mail, birthday) values (31, 'Catlee', 'cstrettleu@archive.org', '2000/01/15');
+insert into employes (id, name, e_mail, birthday) values (32, 'Moises', 'mwentworthv@51.la', '1995/02/16');
+insert into employes (id, name, e_mail, birthday) values (33, 'Cad', 'cmabenw@hubpages.com', '1911/08/28');
+insert into employes (id, name, e_mail, birthday) values (34, 'Thomasina', 'tduvalx@bandcamp.com', '1903/05/26');
+insert into employes (id, name, e_mail, birthday) values (35, 'Neysa', 'nkepey@senate.gov', '1984/01/10');
+insert into employes (id, name, e_mail, birthday) values (36, 'Leopold', 'lkettlestringez@admin.ch', '1980/02/21');
+insert into employes (id, name, e_mail, birthday) values (37, 'Andre', 'acapineer10@marketwatch.com', '1957/03/20');
+insert into employes (id, name, e_mail, birthday) values (38, 'Moishe', 'mcownden11@google.com.br', '1928/08/04');
+insert into employes (id, name, e_mail, birthday) values (39, 'Hercules', 'hjardein12@oakley.com', '1901/11/04');
+insert into employes (id, name, e_mail, birthday) values (40, 'Sib', 'sheap13@gizmodo.com', '1904/12/21');
+insert into employes (id, name, e_mail, birthday) values (41, 'Gilberto', 'gboddam14@github.io', '1918/01/30');
+insert into employes (id, name, e_mail, birthday) values (42, 'Trudi', 'tbrigdale15@cyberchimps.com', '1966/05/31');
+insert into employes (id, name, e_mail, birthday) values (43, 'Jorie', 'jastill16@uiuc.edu', '1953/09/15');
+insert into employes (id, name, e_mail, birthday) values (44, 'Dennison', 'dfall17@yellowpages.com', '1996/05/01');
+insert into employes (id, name, e_mail, birthday) values (45, 'Oralie', 'obibey18@printfriendly.com', '1912/05/26');
+insert into employes (id, name, e_mail, birthday) values (46, 'Tana', 'tfetherston19@mediafire.com', '1982/01/22');
+insert into employes (id, name, e_mail, birthday) values (47, 'Teri', 'tgoldney1a@biglobe.ne.jp', '1932/07/12');
+insert into employes (id, name, e_mail, birthday) values (48, 'Web', 'wpinch1b@netvibes.com', '1936/08/31');
+insert into employes (id, name, e_mail, birthday) values (49, 'Eleonora', 'easals1c@oaic.gov.au', '1969/04/29');
+insert into employes (id, name, e_mail, birthday) values (50, 'Mic', 'mshreve1d@cbsnews.com', '1946/08/08');
+insert into employes (id, name, e_mail, birthday) values (51, 'Brett', 'bbroome1e@shutterfly.com', '1918/10/10');
+insert into employes (id, name, e_mail, birthday) values (52, 'Henri', 'hcomi1f@who.int', '1917/10/31');
+insert into employes (id, name, e_mail, birthday) values (53, 'Zorana', 'ztschersich1g@csmonitor.com', '1914/04/28');
+insert into employes (id, name, e_mail, birthday) values (54, 'Ferdinanda', 'fdymoke1h@pinterest.com', '1937/11/18');
+insert into employes (id, name, e_mail, birthday) values (55, 'Gilberte', 'gtyrone1i@vk.com', '1970/06/19');
+insert into employes (id, name, e_mail, birthday) values (56, 'Luce', 'laked1j@woothemes.com', '1986/11/18');
+insert into employes (id, name, e_mail, birthday) values (57, 'Lurette', 'lmckerrow1k@buzzfeed.com', '1968/12/04');
+insert into employes (id, name, e_mail, birthday) values (58, 'Tawnya', 'tblanckley1l@jimdo.com', '1906/11/27');
+insert into employes (id, name, e_mail, birthday) values (59, 'Michelle', 'mosheerin1m@hp.com', '1995/01/20');
+insert into employes (id, name, e_mail, birthday) values (60, 'Jarvis', 'jsoda1n@gnu.org', '1995/10/20');
+insert into employes (id, name, e_mail, birthday) values (61, 'Auria', 'asmallcombe1o@360.cn', '1918/06/24');
+insert into employes (id, name, e_mail, birthday) values (62, 'Dodie', 'dhaverty1p@github.io', '1975/10/07');
+insert into employes (id, name, e_mail, birthday) values (63, 'Andonis', 'ahilling1q@chronoengine.com', '1939/11/13');
+insert into employes (id, name, e_mail, birthday) values (64, 'Catherina', 'cabramovitz1r@comsenz.com', '1925/07/27');
+insert into employes (id, name, e_mail, birthday) values (65, 'Hunt', 'habbatt1s@sina.com.cn', '1934/02/28');
+insert into employes (id, name, e_mail, birthday) values (66, 'Ody', 'omordecai1t@toplist.cz', '1918/09/30');
+insert into employes (id, name, e_mail, birthday) values (67, 'Morly', 'msorley1u@1688.com', '1910/08/18');
+insert into employes (id, name, e_mail, birthday) values (68, 'Kayne', 'klonsbrough1v@sogou.com', '1930/08/21');
+insert into employes (id, name, e_mail, birthday) values (69, 'Darlleen', 'djailler1w@instagram.com', '1967/08/09');
+insert into employes (id, name, e_mail, birthday) values (70, 'Erie', 'ecarling1x@umn.edu', '1952/05/04');
+insert into employes (id, name, e_mail, birthday) values (71, 'Judith', 'jkorneichuk1y@infoseek.co.jp', '1995/06/10');
+insert into employes (id, name, e_mail, birthday) values (72, 'Juan', 'jgrealy1z@usda.gov', '1906/05/28');
+insert into employes (id, name, e_mail, birthday) values (73, 'Keven', 'kpearmain20@mozilla.org', '1974/08/08');
+insert into employes (id, name, e_mail, birthday) values (74, 'Layne', 'lclutram21@github.io', '1967/11/29');
+insert into employes (id, name, e_mail, birthday) values (75, 'Kaia', 'klightbowne22@reference.com', '1939/09/07');
+insert into employes (id, name, e_mail, birthday) values (76, 'Devlin', 'dbloxsom23@geocities.jp', '1926/12/20');
+insert into employes (id, name, e_mail, birthday) values (77, 'Osborn', 'oskellon24@wordpress.org', '1947/05/19');
+insert into employes (id, name, e_mail, birthday) values (78, 'Janela', 'jpascow25@instagram.com', '1959/03/06');
+insert into employes (id, name, e_mail, birthday) values (79, 'Matilda', 'mgascar26@google.pl', '1977/03/11');
+insert into employes (id, name, e_mail, birthday) values (80, 'Brigitte', 'btomankiewicz27@sciencedaily.com', '1986/02/24');
+insert into employes (id, name, e_mail, birthday) values (81, 'Shane', 'sbartlett28@bizjournals.com', '1951/12/16');
+insert into employes (id, name, e_mail, birthday) values (82, 'Ryley', 'rharler29@whitehouse.gov', '1944/12/19');
+insert into employes (id, name, e_mail, birthday) values (83, 'Fanechka', 'fviel2a@state.gov', '1907/12/23');
+insert into employes (id, name, e_mail, birthday) values (84, 'Ikey', 'istilgoe2b@netlog.com', '1948/01/06');
+insert into employes (id, name, e_mail, birthday) values (85, 'Oran', 'ofidgett2c@reddit.com', '1930/06/21');
+insert into employes (id, name, e_mail, birthday) values (86, 'Michaeline', 'mthunder2d@amazon.co.jp', '1924/06/23');
+insert into employes (id, name, e_mail, birthday) values (87, 'Bale', 'bcantu2e@rakuten.co.jp', '1973/02/22');
+insert into employes (id, name, e_mail, birthday) values (88, 'Ransell', 'rhaddick2f@bbb.org', '1979/06/24');
+insert into employes (id, name, e_mail, birthday) values (89, 'Arlen', 'acorbie2g@wiley.com', '1931/03/09');
+insert into employes (id, name, e_mail, birthday) values (90, 'Farlee', 'fdesesquelle2h@un.org', '1937/08/10');
+insert into employes (id, name, e_mail, birthday) values (91, 'Benedicto', 'bbarends2i@sitemeter.com', '1953/04/23');
+insert into employes (id, name, e_mail, birthday) values (92, 'Jarvis', 'jcamplin2j@epa.gov', '1931/10/24');
+insert into employes (id, name, e_mail, birthday) values (93, 'Catha', 'cbudnk2k@pcworld.com', '1943/03/01');
+insert into employes (id, name, e_mail, birthday) values (94, 'Peadar', 'phaysman2l@buzzfeed.com', '1936/08/22');
+insert into employes (id, name, e_mail, birthday) values (95, 'Evvie', 'ejacombs2m@webnode.com', '1974/04/28');
+insert into employes (id, name, e_mail, birthday) values (96, 'Katuscha', 'kowbridge2n@ca.gov', '1976/06/03');
+insert into employes (id, name, e_mail, birthday) values (97, 'Lyndel', 'lgiannassi2o@dot.gov', '1953/12/10');
+insert into employes (id, name, e_mail, birthday) values (98, 'Dewey', 'dmoorerud2p@jalbum.net', '1903/09/20');
+insert into employes (id, name, e_mail, birthday) values (99, 'Bibbye', 'btevelov2q@sciencedaily.com', '1933/12/11');
+insert into employes (id, name, e_mail, birthday) values (100, 'Sayre', 'sgostridge2r@sogou.com', '1929/08/06');*/
+/*UPDATE employes 
+ SET name='Jeferson',
+ 	 birthday='1997-10-07',
+	 e_mail='jeff@gmail.com'
+	WHERE id='10';
+UPDATE employes 
+ SET name='Gold',
+ 	 birthday='1997-9-07',
+	 e_mail='gold@gmail.com'
+	 WHERE id='11';
+	 
+	 
+UPDATE employes 
+ SET name='abarama',
+ 	 birthday='1992-9-07',
+	 e_mail='abarama@gmail.com'
+	 WHERE id='18';
+ 
+UPDATE employes 
+
+ SET name='corleone',
+ 	 birthday='1987-9-07',
+	 e_mail='corleone@gmail.com'
+	 WHERE id='25';	
+UPDATE employes 
+ SET name='memis',
+ 	 birthday='1985-9-07',
+	 e_mail='memis@gmail.com'
+	 WHERE id='8';*/
+--DELETE FROM employes
+--WHERE id='34';
+	
